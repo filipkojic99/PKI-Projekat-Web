311176551c9b0825b1d33c0064fd0aa65b219556
Filip Kojić <filipkojic48@gmail.com>
2024-01-06 23:03:11 +0100

    dodato automatsko smenjivanje

--- src/app/kupac-pocetna/kupac-pocetna.component.ts
+++ src/app/kupac-pocetna/kupac-pocetna.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Proizvod } from 'src/models/proizvod';
-import { KorisnikService } from 'src/services/korisnik.service';
 import { ProizvodService } from 'src/services/proizvod.service';
 
 import { trigger, state, style, transition, animate } from '@angular/animations';
@@ -13,13 +12,10 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   styleUrls: ['./kupac-pocetna.component.css'],
   animations: [
     trigger('fadeInOut', [
-      state('in', style({ opacity: 1 })),
       transition(':enter', [
         style({ opacity: 0 }),
-        animate(600 )
-      ]),
-      transition(':leave',
-        animate(600, style({ opacity: 0 })))
+        animate('2000ms ease-in-out', style({ opacity: 1 }))
+      ])
     ])
   ]
 })
@@ -29,48 +25,25 @@ export class KupacPocetnaComponent implements OnInit, OnDestroy {
   trenutnaPromocija: Proizvod;
   trenutnaPromocijaIndex: number = 0;
   intervalId: any;
-  stateExpression: string;
 
   constructor(private proizvodService: ProizvodService, private toastr: ToastrService,
-    private router: Router) { }
+              private router: Router) { }
 
-    ngOnInit(): void {
-      this.promocije = this.proizvodService.dohvatiProizvode();
-      this.updatePromotionDisplay();
-      this.rotirajPromocije();
-    }
-    
-    updatePromotionDisplay(): void {
-      this.stateExpression = 'in'; // Set the initial state
-      this.trenutnaPromocija = this.promocije[this.trenutnaPromocijaIndex];
-    }
-    
-    rotirajPromocije(): void {
-      this.intervalId = setInterval(() => {
-        this.sledecaPromocija();
-        this.updatePromotionDisplay(); // Update the displayed promotion
-      }, 5000);
-    }
-    
-
-  sledecaPromocija(): void {
-    this.trenutnaPromocijaIndex = (this.trenutnaPromocijaIndex + 1) % this.promocije.length;
-    this.stateExpression = 'in'; // This will reset the state and trigger the animation
-  }
-  
-  prethodnaPromocija(): void {
-    this.trenutnaPromocijaIndex = (this.trenutnaPromocijaIndex - 1 + this.promocije.length) % this.promocije.length;
-    this.stateExpression = 'in'; // This will reset the state and trigger the animation
-  }
-  
-
-  applyTransitionEffect(): void {
-    // Apply your transition effect logic here, for example, fade in/out
-    // You can implement this with Angular animations or by toggling CSS classes
+  ngOnInit(): void {
+    this.promocije = this.proizvodService.dohvatiProizvode();
+    this.trenutnaPromocija = this.promocije[this.trenutnaPromocijaIndex];
+    this.rotirajPromocije();
   }
 
-  changeAnimationState() {
-    this.stateExpression = this.stateExpression === 'state1' ? 'state2' : 'state1';
+  rotirajPromocije(): void {
+    this.intervalId = setInterval(() => {
+      this.trenutnaPromocijaIndex = (this.trenutnaPromocijaIndex + 1) % this.promocije.length;
+      this.trenutnaPromocija = null; // Ukloni trenutnu promociju
+
+      setTimeout(() => {
+        this.trenutnaPromocija = this.promocije[this.trenutnaPromocijaIndex];
+      }, 0); // Ponovo postavi promociju nakon kratkog intervala
+    }, 5000);
   }
 
   ngOnDestroy(): void {
@@ -78,5 +51,4 @@ export class KupacPocetnaComponent implements OnInit, OnDestroy {
       clearInterval(this.intervalId);
     }
   }
-
 }
